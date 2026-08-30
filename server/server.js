fef5b4aa75aa8ca7eb00ad415e711c27c6dfaaa4
Vartika Dhochak <dhochakvartika@gmail.com>
2026-08-30 14:31:06 +0530

    Connect API routes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,13 @@
 import express from "express";
 import cors from "cors";
 
+import userRoute from "./routes/userRoute.js";
+import productRoute from "./routes/productRoute.js";
+import cartRoute from "./routes/cartRoute.js";
+import orderRoute from "./routes/orderRoute.js";
+import addressRoute from "./routes/addressRoute.js";
+import sellerRoute from "./routes/sellerRoute.js";
+
 const app = express();
 
 const allowedOrigins = [
@@ -21,11 +28,23 @@ app.use(cors({
 
 app.use(express.json());
 
+
+// API ROUTES
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/cart", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/address", addressRoute);
+app.use("/api/seller", sellerRoute);
+
+
+// Test route
 app.get("/", (req, res) => {
     res.json({
         message: "GreenCart API is running"
     });
 });
+
 
 const PORT = process.env.PORT || 5000;
 
