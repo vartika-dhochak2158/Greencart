bd961e15d7ebb3590097fdd50343ea1d0e2ba179
Vartika Dhochak <dhochakvartika@gmail.com>
2026-09-11 12:45:48 +0530

    Fix backend middleware and API routes

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import connectDB from "./configs/db.js";
+import cookieParser from "cookie-parser";
 
 import userRoute from "./routes/userRoute.js";
 import productRoute from "./routes/productRoute.js";
@@ -29,6 +30,7 @@ app.use(cors({
 }));
 
 app.use(express.json());
+app.use(cookieParser());
 
 
 // API ROUTES
