41dfd979ee83aaa2be239a119167a6c4a05b6d34
Vartika Dhochak <dhochakvartika@gmail.com>
2026-09-11 11:53:38 +0530

    Fix backend environment and MongoDB connection

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import connectDB from "./configs/db.js";
 
 import userRoute from "./routes/userRoute.js";
 import productRoute from "./routes/productRoute.js";
@@ -47,6 +49,8 @@ app.get("/", (req, res) => {
 
 
 const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
